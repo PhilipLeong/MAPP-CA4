803b07d6fd66856a7d13fdd628c4e585d6c50956
PhilipLeong <philipleong.18@ichat.sp.edu.sg>
2020-01-19 20:26:36 +0800

    Login and Signup

--- BookHub/app/src/main/java/mapp/com/sg/bookhub/MainActivity.java
+++ BookHub/app/src/main/java/mapp/com/sg/bookhub/MainActivity.java
@@ -1,13 +1,97 @@
 package mapp.com.sg.bookhub;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import mapp.com.sg.bookhub.loginsignupui.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
+    private TabLayout tabLayout;
+    private ProgressDialog progessDialog;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progessDialog = new ProgressDialog(this);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+
+
+    }
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //Email is empty
+            Toast.makeText(this, "Pease enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            //Password is empty
+            Toast.makeText(this,"Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progessDialog.setMessage("Registering User...");
+        progessDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Unsuccessful, please try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view){
+          if(view == buttonRegister ){
+              registerUser();
+          }
+          if(view == textViewSignin){
+              //Open Login Activity here
+          }
     }
 }
---
+++ BookHub/app/src/main/java/mapp/com/sg/bookhub/loginsignupui/SignUpFragment.java
@@ -0,0 +1,20 @@
+package mapp.com.sg.bookhub.loginsignupui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import mapp.com.sg.bookhub.R;
+
+public class SignUpFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.signupfragment, container, false);
+    }
+}
