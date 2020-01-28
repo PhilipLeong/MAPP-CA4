1845ab55c944932a4e109d73df80bd4b59595751
PhilipLeong <philipleong.18@ichat.sp.edu.sg>
2020-01-28 18:16:38 +0800

    Added Post Functionality

--- BookHub/app/src/main/java/mapp/com/sg/bookhub/HomeActivity.java
+++ BookHub/app/src/main/java/mapp/com/sg/bookhub/HomeActivity.java
@@ -1,14 +1,29 @@
 package mapp.com.sg.bookhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private ImageButton postbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        postbtn = (ImageButton) findViewById(R.id.post_ImgBtn);
+
+        postbtn.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        if(view == postbtn) {
+            Intent intent = new Intent(this, PostActivity.class);
+            startActivity(intent);
+        }
     }
 }
