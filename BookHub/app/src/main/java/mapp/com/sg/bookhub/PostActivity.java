03c9062d0b49bac2a0bf39e7292d659a8bf18ef5
leileiJng <jlei99.18@ichat.sp.edu.sg>
2020-01-30 17:27:26 +0800

    post save

--- BookHub/app/src/main/java/mapp/com/sg/bookhub/PostActivity.java
+++ BookHub/app/src/main/java/mapp/com/sg/bookhub/PostActivity.java
@@ -1,9 +1,13 @@
 package mapp.com.sg.bookhub;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -19,29 +23,45 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.lang.reflect.Array;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import mapp.com.sg.bookhub.Models.Post;
-import mapp.com.sg.bookhub.Models.User;
 
-public class PostActivity extends  AppCompatActivity implements View.OnClickListener {
+
+public class PostActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView pageTitle;
     private ImageButton backBtn;
     private Button postBtn;
+
+    //error pop-up
+    private Dialog errorDialog;
+    private ImageButton errorCross;
+    private TextView errorTV;
+
+    //success pop-up
+    private Dialog successDialog;
+    private ImageButton successCross;
+    private TextView successTV;
+
+    private ProgressDialog progessDialog;
 
     private EditText title;
     private EditText condition;
@@ -60,8 +80,13 @@ public class PostActivity extends  AppCompatActivity implements View.OnClickList
     private String userId;
     private List<String> payments;
 
+    private ImageButton add1;
+    private ImageButton add2;
+    private ImageButton add3;
+
     private FirebaseFirestore db;
     private static final String TAG = "PostActivity";
+    int TAKE_IMAGE_CODE =  10001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,23 +123,34 @@ public class PostActivity extends  AppCompatActivity implements View.OnClickList
 
         payments = new ArrayList<>();
 
+        add1 = (ImageButton) findViewById(R.id.addImgBtn1);
+        add2 = (ImageButton) findViewById(R.id.addImgBtn2);
+        add3 = (ImageButton) findViewById(R.id.addImgBtn3);
+        add1.setOnClickListener(this);
+        add2.setOnClickListener(this);
+        add3.setOnClickListener(this);
+
+        errorDialog = new Dialog(this);
+        errorDialog.setContentView(R.layout.custompopup);
+        errorCross = errorDialog.findViewById(R.id.exit_btn);
+        errorCross.setOnClickListener(this);
+        errorTV = errorDialog.findViewById(R.id.error_TV);
+
+        progessDialog = new ProgressDialog(this);
     }
+
+
     private void backToMainPage(){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 
+
+
     private void makepost(){
-        String titleInput = title.getText().toString().trim();
-        String conditionInput = condition.getText().toString().trim();
-        Double massInput = Double.parseDouble(mass.getText().toString().trim());
-        Double priceInput = Double.parseDouble(price.getText().toString().trim());
-        String locationInput = location.getText().toString().trim();
-        String scheduleInput = schedule.getText().toString().trim();
+        Double massFinal = 0.0;
+        Double priceFinal = 0.0;
         String schoolInput;
-        boolean validation = true;
-
-
         //Get and validate radio button, (School input)
         int selectedId = radioGroup1.getCheckedRadioButtonId();
         if(selectedId != -1){
@@ -134,8 +170,7 @@ public class PostActivity extends  AppCompatActivity implements View.OnClickList
                     schoolInput = radioSelected.getText().toString();
                 }
                 else{
-                    //school not selected, validation, error message
-                    validation = false;
+                    showErrorPopup("Please select the school :).");
                     return;
                 }
             }
@@ -153,33 +188,90 @@ public class PostActivity extends  AppCompatActivity implements View.OnClickList
         if (chkpaynow.isChecked()){
             payments.add(chkpaynow.getText().toString());
         }
+        if (payments.size() == 0){
+            showErrorPopup("Please check at least one payment method.");
+            return;
+        }
+
+        String titleInput = title.getText().toString().trim();
+        String conditionInput = condition.getText().toString().trim();
+        String massInput = mass.getText().toString().trim();
+        String priceInput = price.getText().toString().trim();
+        String locationInput = location.getText().toString().trim();
+        String scheduleInput = schedule.getText().toString().trim();
 
 
-        if(TextUtils.isEmpty(titleInput) || TextUtils.isEmpty(conditionInput) || TextUtils.isEmpty(scheduleInput) || TextUtils.isEmpty(locationInput)){
-            //something to be done
-            validation = false;
+        if(TextUtils.isEmpty(titleInput) || TextUtils.isEmpty(conditionInput) || TextUtils.isEmpty(scheduleInput) || TextUtils.isEmpty(locationInput)|| TextUtils.isEmpty(massInput)|| TextUtils.isEmpty(priceInput) ){
+            showErrorPopup("Please don't leave any field blank.");
+            return;
+        }
+
+        try{
+            massFinal = Double.parseDouble(massInput);
+            priceFinal = Double.parseDouble(priceInput);
+        }
+        catch(Exception ex){
+            showErrorPopup("Please provide valid Mass / Price input.");
             return;
         }
 
 
         //start input into database
-        Post newPost = new Post (titleInput, conditionInput, massInput, priceInput, locationInput, scheduleInput, schoolInput, payments, userId);
+        Post newPost = new Post (titleInput, conditionInput, massFinal, priceFinal, locationInput, scheduleInput, schoolInput, payments, userId);
+
+        progessDialog.setMessage("Posting in progress...");
+        progessDialog.show();
 
         db.collection("Posts").document().set(newPost).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                Log.d(TAG, "DocumentSnapshot successfully written!");
+                progessDialog.dismiss();
+                //Toast.makeText(getApplicationContext(),"You have made a post! ",Toast.LENGTH_SHORT);
+                Log.d(TAG, "Make a post successfully!");
             }
         })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error writing document", e);
+                        Log.w(TAG, "Error happened in posting", e);
                     }
                 });
         ;
 
     }
+
+
+
+    private void uploadBookPic(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getApplicationContext().getPackageManager()) != null){
+            startActivityForResult(intent, TAKE_IMAGE_CODE);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ( requestCode == TAKE_IMAGE_CODE){
+            switch(resultCode){
+                case Activity.RESULT_OK:
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    add1.setImageBitmap(bitmap);
+                    //handleUpload(bitmap);
+            }
+        }
+    }
+
+
+
+    private void showErrorPopup(String errorMessage){
+        errorDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        errorTV.setText(errorMessage);
+        errorDialog.show();
+    }
+
+
 
     public void onClick(View view){
         if(view == backBtn){
@@ -187,6 +279,12 @@ public class PostActivity extends  AppCompatActivity implements View.OnClickList
         }
         if(view == postBtn){
             makepost();
+        }
+        if(view == add1 || view == add2 || view == add3){
+            uploadBookPic();
+        }
+        if(view == errorCross){
+            errorDialog.dismiss();
         }
     }
 }
