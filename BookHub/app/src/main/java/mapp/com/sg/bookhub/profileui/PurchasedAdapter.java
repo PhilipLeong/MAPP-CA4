9c42c83fedfab8d22d7f6220c3a9e150a387ffa1
PhilipLeong <philipleong.18@ichat.sp.edu.sg>
2020-02-07 22:57:13 +0800

    Purchased Profile Functionality

---
+++ BookHub/app/src/main/java/mapp/com/sg/bookhub/profileui/PurchasedAdapter.java
@@ -0,0 +1,111 @@
+package mapp.com.sg.bookhub.profileui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import mapp.com.sg.bookhub.BookDetailsActivity;
+import mapp.com.sg.bookhub.Models.Post;
+import mapp.com.sg.bookhub.R;
+
+public class PurchasedAdapter extends RecyclerView.Adapter<PurchasedAdapter.ViewHolder> implements View.OnClickListener{
+
+    private List<Post> postItems;
+    private Context context;
+    private static DecimalFormat df2 = new DecimalFormat("##.00");
+
+    public PurchasedAdapter(List<Post> posts, Context context){
+        this.postItems = posts;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.profilepurchased_cardview, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final PurchasedAdapter.ViewHolder holder, int position) {
+        final Post postItem = postItems.get(position);
+        holder.titleTV.setText(postItem.getTitle());
+        holder.isbnTV.setText(postItem.getIsbn());
+
+        String price = df2.format(postItem.getPrice());
+        holder.priceTV.setText("SGD " + price);
+        //holder.titleTV.setText(postItem.getTitle());
+        holder.morebtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("!!Store page click:", postItem.getTitle());
+                Intent intent = new Intent(context, BookDetailsActivity.class);
+                intent.putExtra("BOOK", postItem);
+                context.startActivity(intent);
+            }
+        });
+
+        StorageReference downloadURL = FirebaseStorage.getInstance().getReferenceFromUrl(postItem.getImgs().get(0));
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        downloadURL.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                holder.previewImg.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postItems.size();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView titleTV;
+        public TextView isbnTV;
+        public TextView priceTV;
+        public ImageButton morebtn;
+        public ImageView previewImg;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleTV = (TextView) itemView.findViewById(R.id.booktitle_TV);
+            isbnTV = (TextView) itemView.findViewById(R.id.isbn_TV);
+            priceTV = (TextView) itemView.findViewById(R.id.price_TV);
+            morebtn = (ImageButton) itemView.findViewById(R.id.more_Btn);
+            previewImg = (ImageView) itemView.findViewById(R.id.bookimage_IV);
+        }
+    }
+}
--- BookHub/app/src/main/java/mapp/com/sg/bookhub/profileui/PurchasedFragment.java
+++ BookHub/app/src/main/java/mapp/com/sg/bookhub/profileui/PurchasedFragment.java
@@ -1,19 +1,126 @@
 package mapp.com.sg.bookhub.profileui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mapp.com.sg.bookhub.Models.Post;
 import mapp.com.sg.bookhub.R;
 
 public class PurchasedFragment extends Fragment {
+
+    private String selectedSchool;
+    private final String TAG = "Purchased";
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private TextView pageHeader;
+
+    private List<Post> posts;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
+
+    private String userid;
+    private ImageButton backBtn;
+
+    private Button profileBtn;
+    private Button storeBtn;
+    private ImageButton postBtn;
+    private List<String> orders;
+
+    private ImageView nobookImage;
+    private TextView nobookText;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.purchasedfragment, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        userid = firebaseAuth.getCurrentUser().getUid();
+
+        nobookImage = (ImageView) rootView.findViewById(R.id.nobook_IV);
+        nobookText = (TextView) rootView.findViewById(R.id.nobook_TV);
+
+        profileBtn = (Button) rootView.findViewById(R.id.profile_Btn);
+        storeBtn = (Button) rootView.findViewById(R.id.store_Btn);
+        postBtn = (ImageButton) rootView.findViewById(R.id.post_Btn);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recylerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        pageHeader = (TextView)rootView.findViewById(R.id.pageTitle_TV);
+        posts = new ArrayList<Post>();
+
+        Log.d("test","Works!");
+        Log.d("Userid",userid);
+        db.collection("Orders")
+                .whereEqualTo("createdBy", userid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot documents = task.getResult();
+                            Log.d("Documents", ""+documents.size());
+                            if (documents.size() != 0) {
+                                for (QueryDocumentSnapshot document : documents) {
+                                    String order = document.getData().get("postId").toString();
+                                    Log.d("Order: ",order);
+                                    DocumentReference docRef = db.collection("Posts").document(order);
+                                    docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                            Post p = documentSnapshot.toObject(Post.class);
+                                            posts.add(p);
+                                            startRecycleView(posts);
+                                        }
+                                    });
+                                }
+                            }
+                            else{
+                                recyclerView.setVisibility(View.GONE);
+                                Log.d(TAG,"No purchased books");
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
         return rootView;
     }
+
+    private void startRecycleView (List<Post> posts){
+        adapter = new PurchasedAdapter(posts, getContext());
+        Log.d("Adapter",adapter.toString());
+        recyclerView.setAdapter(adapter);
+    }
+
 }
